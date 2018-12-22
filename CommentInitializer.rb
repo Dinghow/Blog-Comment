b013370b9d0d930e02679aa2e370c1a74b37bd55
DinghowYang <DinghowYang@gmail.com>
2018-12-22 22:58:50 +0800

    Upload a script to create the issues automatically

---
+++ CommentInitializer.rb
@@ -0,0 +1,30 @@
+username = "Dinghow" # GitHub 用户名
+new_token = "d398c1c31215cc1dc2f2d6fe8c2830dd758fdb5f"  # GitHub Token
+repo_name = "Blog-Comment" # 存放 issues
+sitemap_url = "https://dinghow.site/sitemap.xml" # sitemap
+kind = "Gitalk" # "Gitalk" or "gitment"
+
+require 'open-uri'
+require 'faraday'
+require 'active_support'
+require 'active_support/core_ext'
+require 'sitemap-parser'
+require 'digest'
+
+md5 = Digest::MD5.new
+sitemap = SitemapParser.new sitemap_url
+urls = sitemap.to_a
+
+conn = Faraday.new(:url => "https://api.github.com/repos/#{username}/#{repo_name}/issues") do |conn|
+  conn.basic_auth(username, new_token)
+  conn.adapter  Faraday.default_adapter
+end
+
+urls.each_with_index do |url, index|
+  title = open(url).read.scan(/<title>(.*?)<\/title>/).first.first.force_encoding('UTF-8')
+  response = conn.post do |req|
+    req.body = { body: url, labels: [kind, md5.hexdigest(url)], title: title }.to_json
+  end
+  puts response.body
+  sleep 15 if index % 20 == 0
+end
